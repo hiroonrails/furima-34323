2193a9cf115a2f52bf25081115fdd13d9349c975
hiroonrails <m69lover.scachaaan@gmail.com>
2021-05-01 18:42:06 +0900

    usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         validates :name, presence: true
+         validates :kanji_first, presence: true
+         validates :kanji_last, presence: true
+         validates :kana_first, presence: true
+         validates :kana_last, presence: true
+         validates :birth_date, presence: true
 end
--- db/migrate/20210501085213_devise_create_users.rb
+++ db/migrate/20210501085213_devise_create_users.rb
@@ -4,8 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name,               null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :kanji_first,        null: false
+      t.string :kanji_last,         null: false
+      t.string :kana_first,         null: false
+      t.string :kana_last,          null: false
+      t.date   :birth_date,         null: false
 
       ## Recoverable
       t.string   :reset_password_token
