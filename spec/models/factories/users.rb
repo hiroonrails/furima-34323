049c1070a5e07229face59c0261a145a241fd4cf
hiroonrails <m69lover.scachaaan@gmail.com>
2021-05-05 22:53:19 +0900

    factoryとgimeiのインストール

--- spec/models/factories/users.rb
+++ spec/models/factories/users.rb
@@ -1,8 +1,16 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    email                 {'test@example'}
-    password              {'000000'}
+    transient do
+      person{Gimei.name}
+    end
+    name                  {Faker::JapaneseMedia::StudioGhibli.character}
+    email                 {FAKER::Internet.free_email}
+    password              {FAKER::Internet.password(min_length: 6)}
     password_confirmation {password}
+    kanji_first           {person.first.kanji}
+    kanji_last            {person.last.kanji}
+    kana_first            {person.first.kana}
+    kana_last             {person.last.kana}
+    birth_date            {FAKER::Date.backward}
   end
 end
