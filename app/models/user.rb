2d1f9e4c2f6948a751469c6f763d4406880b2fd3
hiroonrails <m69lover.scachaaan@gmail.com>
2021-05-07 21:07:56 +0900

    テストコード(修正4回目？)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         # with_options使って中身を記載すると全部に適用される。
          VALID_REGEX = /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}\z/
         with_options presence: true do
           validates :name
-          #このオプションは使える。format: { with: REGEX(正規表現) , message "表示したいこと"}
           validates :password, format: { with: VALID_REGEX , message: "は6文字以上の英数字が使えます" }
-          # with_optionsもわんちゃん入れ子構造になるかもしれん
           with_options format: { with: /\A[一-龥]+\z/, message: '全角漢字の文字を使用してください' } do
             validates :kanji_first
             validates :kanji_last
