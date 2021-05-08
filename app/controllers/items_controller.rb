c944b1a610fc42ff94c9b3130d0533d212b46aea
hiroonrails <m69lover.scachaaan@gmail.com>
2021-05-08 13:41:09 +0900

    ControllerとValidatesの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,14 +4,25 @@ class ItemsController < ApplicationController
 
   def new
     if user_signed_in?
-      @items = Item.new
+      @item = Item.new
     else
-      redirect_to root_path
+      redirect_to user_session_path
     end
   end
 
   def create
+    @item = Item.new(set_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
+  private
+
+  def set_params
+    params.require(:item).permit(:item_name, :explanation, category_id, status_id, delivery_fee_id, prefecture_id, shipment_day_id, price).merge(current_user.id[:user_id])
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :item_name
+    validates :explanation
+    validates :category_id
+    validates :status_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :shipment_day_id
+    validates :price
+    validates :image
+  end
 end
