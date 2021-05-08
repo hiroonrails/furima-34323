ab9fbf703b6a3f826b0dfcfd305c6ddae1666e60
hiroonrails <m69lover.scachaaan@gmail.com>
2021-05-08 12:38:59 +0900

    ログインユーザーのみ出品ページ遷移&items table作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    if user_signed_in?
+      @items = Item.new
+    else
+      redirect_to root_path
+    end
+  end
+
+  def create
+
+  end
+
 end
