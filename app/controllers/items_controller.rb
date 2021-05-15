34085abd1be4cb6539f6c5b0235bb41c4b5fbe46
hiroonrails <m69lover.scachaaan@gmail.com>
2021-05-15 17:13:47 +0900

    商品情報編集機能です

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -22,10 +23,32 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(set_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def set_params
     params.require(:item).permit(:item_name, :explanation, :category_id, :status_id, :delivery_fee_id, :prefecture_id,
                                  :shipment_day_id, :price, :image).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    else
+      redirect_to root_path
+    end
+  end
+
 end
