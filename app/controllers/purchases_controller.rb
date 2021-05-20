b00b0ea28c1dae89a18ad9e62d9712a9c04c893a
hiroonrails <m69lover.scachaaan@gmail.com>
2021-05-20 22:13:10 +0900

    ビューファイルの直し

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,8 +1,24 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
     @purchase_address = PurchaseAddress.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @purchase_address = PurchaseAddress.new(payment_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
+
+  private
+  def payment_params
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :address_line1, :address_line2, :bldg_name, :phone_number, :item_id).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,11 +1,20 @@
 class PurchaseAddress
 
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :address_line1, :address_line2, :bldg_name, :phone_number, :user_id, :item_id, :purchase_id
+  attr_accessor :postal_code, :prefecture_id, :address_line1, :address_line2, :bldg_name, :phone_number, :user_id, :item_id
 
-  # ここにバリデーションの処理を書く
+  with_options presence: true do
+    validates :postal_code
+    validates :prefecture_id
+    validates :address_line1
+    validates :address_line2
+    validates :bldg_name
+    validates :phone_number
+    validates :user_id
+    validates :item_id
+  end
 
   def save
-    # 各テーブルにデータを保存する処理を書く
+    :purchase_id
   end
 end
