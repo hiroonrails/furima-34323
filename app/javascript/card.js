7354ee25fb781813b88d7f9f2fa71eb882329759
hiroonrails <m69lover.scachaaan@gmail.com>
2021-05-22 18:37:15 +0900

    JS動作確認

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,16 +1,17 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_d6240218af77b9fb1e4af848"); // PAY.JPテスト公開鍵
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const form = document.getElementById("charge-form");
-  form.addEventListener("submit", () => {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
     const formResult = document.getElementById("charge-form");
     const formData = new FormData(formResult);
 
     const card = {
-      number: formData.get("card-number"),
-      cvc: formData.get("card-cvc"),
-      exp_month: formData.get("card-exp-month"),
-      exp_year: `20${formData.get("card-exp-year")}`,
+      number: formData.get("purchase_address[number]"),
+      cvc: formData.get("purchase_address[cvc]"),
+      exp_month: formData.get("purchase_address[exp_month]"),
+      exp_year: `20${formData.get("purchase_address[exp_year]")}`,
     };
 
     Payjp.createToken(card, (status, response) => {
