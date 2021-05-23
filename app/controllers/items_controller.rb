9b0db4ca1f1fc7ec52c8638f338bc0b1d0867a9f
hiroonrails <m69lover.scachaaan@gmail.com>
2021-05-23 16:34:47 +0900

    レビュー修正1回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -52,7 +52,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless current_user.id == @item.user_id
+    unless current_user.id == @item.user_id && @item.purchase.blank?
       redirect_to root_path
     end
   end
